aa3e4d2658b3db18194c47d010159e9642b9a632
Raymond Oluoch <rasholuoch@gmail.com>
2024-05-21 12:33:59 +0300

    complete tdd app

---
+++ src/superlists/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from lists import views as list_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", list_views.home_page, name="home"),
+    path("lists/", include("lists.urls")),
+]
